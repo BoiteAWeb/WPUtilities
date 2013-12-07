1d145470c515c0919abb1ff44ce5b5efafc884fd
Julio Potier <juliobosk@gmail.com>
2013-12-07 01:54:49 +0100

    Code opti

--- wp-content/mu-plugins/wpu_meta_tags.php
+++ wp-content/mu-plugins/wpu_meta_tags.php
@@ -2,21 +2,21 @@
 /*
 Plugin Name: WPU Meta tags
 Description: Adds meta tags to the theme header
-Version: 0.2
+Version: 0.3
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
 License URI: http://opensource.org/licenses/MIT
+Contributor: @boiteaweb
+Last Update: 07 dec. 2013
 */
 
 /* ----------------------------------------------------------
   Meta content & open graph
 ---------------------------------------------------------- */
 
-add_action( 'wp_head', 'wpu_user_metas', 0 , 0 );
-
+add_action( 'wp_head', 'wpu_user_metas', 0 );
 function wpu_user_metas() {
-    global $post;
     $metas = array();
 
     $metas['og_sitename'] = array(
@@ -30,11 +30,12 @@ function wpu_user_metas() {
     );
 
     if ( is_single() ) {
+        global $post;
 
         // Meta description
-        $meta_description = str_replace( array( "\n", "\t", '   ', '  ' ), ' ', trim( strip_tags( $post->post_content ) ) );
+        $meta_description = preg_replace( "/\s+/", ' ', strip_tags( strip_shortcodes( $post->post_content ) ) );
         if ( strlen( $meta_description ) > 195 ) {
-            $meta_description = substr( $meta_description, 0, 190 ) . ' ...';
+            $meta_description = substr( $meta_description, 0, 190 ) . ' &hellip;';
         }
 
         $metas['description'] = array(
@@ -49,17 +50,24 @@ function wpu_user_metas() {
             'property' => 'og:url',
             'content' => get_permalink()
         );
-        $metas['og_image'] = array(
-            'property' => 'og:image',
-            'content' => wputh_get_thumbnail_url( 'thumbnail' )
-        );
+        $thumb_id = get_post_thumbnail_id();
+        $thumb_url = wp_get_attachment_image_src( $thumb_id, 'thumbnail', true );
+        if ( isset( $thumb_url[0] ) ) {
+            $metas['og_image'] = array(
+                'property' => 'og:image',
+                'content' => $thumb_url[0]
+            );
+        }
     }
 
-    if ( is_home() ) {
+    if ( is_home() || is_front_page() ) {
         $meta_description = get_bloginfo( 'description' );
+        if ( strlen( $meta_description ) > 195 ) {
+            $meta_description = substr( $meta_description, 0, 190 ) . ' &hellip;';
+        }
         $metas['description'] = array(
             'name' => 'description',
-            'content' => substr( $meta_description, 0, 200 ) . ' ...'
+            'content' => $meta_description
         );
         $metas['og_title'] = array(
             'property' => 'og:title',
@@ -67,48 +75,52 @@ function wpu_user_metas() {
         );
         $metas['og_url'] = array(
             'property' => 'og:url',
-            'content' => site_url()
+            'content' => home_url()
         );
         $metas['og_image'] = array(
             'property' => 'og:image',
-            'content' => get_template_directory_uri() . '/screenshot.png'
+            'content' => 'http://s.wordpress.com/mshots/v1/' . home_url()
         );
     }
 
+    $return = '';
     foreach ( $metas as $values ) {
-        echo '<meta';
+        $return .= '<meta';
         foreach ( $values as $name => $value ) {
-            echo ' '.$name.'="' . $value . '"';
+            $return .= sprintf( ' %s="%s"', $name, esc_attr( $value ) );
         }
-        echo ' />';
+        $return .= ' />'."\n";
     }
+    echo $return;
 }
 
 /* ----------------------------------------------------------
   Robots tag
 ---------------------------------------------------------- */
 
-add_action( 'wp_head', 'wpu_user_metas_robots', 1 , 0 );
-
+add_action( 'wp_head', 'wpu_user_metas_robots', 1 );
 function wpu_user_metas_robots() {
-    global $post;
     $metas = array();
 
-    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-
-    // Disable indexation for archives pages after page 1 OR 404 page
-    if ( ( ( is_category() || is_tag() || is_author() || is_tax() ) && $paged > 1 ) || is_404() ) {
+    // Disable indexation for paginated archives OR 404 page
+    if ( ( is_paged() && ( is_category() || is_tag() || is_author() || is_tax() ) ) || 
+        is_404() ||
+        ( comments_open() && (int)get_query_var( 'cpage' )>0 )
+        ) {
         $metas['robots'] = array(
             'name' => 'robots',
             'content' => 'noindex, follow'
         );
     }
-
-    foreach ( $metas as $values ) {
-        echo '<meta';
-        foreach ( $values as $name => $value ) {
-            echo ' '.$name.'="' . $value . '"';
+    if ( count( $metas ) ) {
+        $return = '';
+        foreach ( $metas as $values ) {
+            $return .= '<meta';
+            foreach ( $values as $name => $value ) {
+                $return .= sprintf( ' %s="%s"', $name, esc_attr( $value ) );
+            }
+            $return .= ' />'."\n";
         }
-        echo ' />';
+        echo $return;
     }
 }
